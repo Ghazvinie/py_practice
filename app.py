b87f9c0a995c55f50d1894390a36302663bfaed2
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-12-16 20:00:21 +0000

    messing about

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-colour = raw_input("Enter a colour ")
-sound = raw_input("Enter a sound ")
-place = raw_input("Enter a place ")
+# colour = raw_input("Enter a colour ")
+# sound = raw_input("Enter a sound ")
+# place = raw_input("Enter a place ")
 
-print("You are a " + place)
-print("The place is a " + sound)
-print("That sound is a " + colour)
+# print("You are a " + place)
+# print("The place is a " + sound)
+# print("That sound is a " + colour)
+
+my_list = [1,2,4,6,7]
+add_me = [8,9,10]
+my_list.extend(add_me)
+my_list.append(12)
+print(my_list)

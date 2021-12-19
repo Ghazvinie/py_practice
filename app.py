b86c33b8042618aecad16472aed1742b2be752f2
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-12-19 17:50:10 +0000

    messing about

--- app.py
+++ app.py
@@ -15,14 +15,30 @@
 # coordinates = (4,5, 'abc')
 # print(coordinates[0])
 
-def sayHi(a):
-    print('hi')
-    print(a)
+# def sayHi(a):
+#     print('hi')
+#     print(a)
 
-    print('why')
+#     print('why')
+
+#     return 'the return'
 
 
+# def isTrue(boolean):
+#     if (boolean): return 'true'
+#     else:
+#         return 'false'
 
+# print(isTrue(False))
 
+dan = 101
 
-sayHi('dhd')
+# if (dan > 18 and dan < 15): print('danny')
+
+# if (dan > 5) and not (dan == 100):
+#     print('greater than 5')
+# elif (dan < 20):
+#     print('less than 20')
+
+if not (dan == 100):
+    print('not equal to 100')

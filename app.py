57e6cf859692ff133400efa0b59653d8ce661222
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-12-19 17:31:05 +0000

    messing about

--- app.py
+++ app.py
@@ -6,8 +6,23 @@
 # print("The place is a " + sound)
 # print("That sound is a " + colour)
 
-my_list = [1,2,4,6,7]
-add_me = [8,9,10]
-my_list.extend(add_me)
-my_list.append(12)
-print(my_list)
+# my_list = [1,2,4,6,7]
+# add_me = [8,9,10]
+# my_list.extend(add_me)
+# my_list.append(12)
+# print(my_list)
+
+# coordinates = (4,5, 'abc')
+# print(coordinates[0])
+
+def sayHi(a):
+    print('hi')
+    print(a)
+
+    print('why')
+
+
+
+
+
+sayHi('dhd')
